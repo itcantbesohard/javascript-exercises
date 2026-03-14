6bf78597ce8755575289326d493f6055e95899f8
Michał Baran <michalbaran@outlook.com>
2026-03-14 12:07:43 +0100

    Solve getTheTitles and findTheOldest tasks

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    return array.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    });
 };
+
+//   {
+// name: "Carly",
+//     yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//       },
 
 // Do not edit below this line
 module.exports = findTheOldest;
